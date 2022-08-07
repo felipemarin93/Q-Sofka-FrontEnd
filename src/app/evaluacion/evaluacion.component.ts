10267687057255f2131297ba1a4c6af77086b0c1
mmaurogg <mmaurogg@gmail.com>
2022-08-06 20:30:07 -0500

    creado componente evaluacion

---
+++ src/app/evaluacion/evaluacion.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluacion',
+  templateUrl: './evaluacion.component.html',
+  styleUrls: ['./evaluacion.component.css']
+})
+export class EvaluacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  enviarEvaluacion(){
+    
+  }
+
+}
