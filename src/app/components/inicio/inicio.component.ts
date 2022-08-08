8f953a155c84a2db3a9626d2bc47288545f592b2
Oscar Farfan <oscarfarfan92@gmail.com>
2022-08-08 18:39:55 -0500

    Se genera funcionalidad para validar credenciales

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
+import { AutenticacionInicioSesionService } from 'src/app/services/autenticacion-inicio-sesion.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  constructor(
+    private autenticacionInicioSesion: AutenticacionInicioSesionService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  verificarCredenciales(nombreUsuario: string) {
+    console.log("verificar")
+    this.autenticacionInicioSesion
+      .obtenerUsuarioPorNombreUsuario(nombreUsuario)
+      .subscribe((usuario: Usuario) => {
+        if (usuario.nombre !== null) {
+          if (usuario.nombre.includes('.')) {
+            this.router.navigate(['coach-dashboard']);
+          } else {
+            alert('El nombre de usuario no es válido');
+          }
+        } else {
+          alert(
+            'Usuario no registrado, contactarse con el superadmin para el registro y entrega de sus credenciales.'
+          );
+        }
+      });
   }
-
 }
