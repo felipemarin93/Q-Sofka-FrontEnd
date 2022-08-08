c2ae055526c92f7b4d90c2c73034fc7aa7d393ef
juan9507 <juandavidnaranjo75@gmail.com>
2022-08-07 20:20:46 -0500

    creacion funcionamiento en eliminar una opcion

--- src/app/components/creacion-preguntas-component/creacion-preguntas-component.component.ts
+++ src/app/components/creacion-preguntas-component/creacion-preguntas-component.component.ts
@@ -4,49 +4,56 @@ import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-creacion-preguntas-component',
   templateUrl: './creacion-preguntas-component.component.html',
-  styleUrls: ['./creacion-preguntas-component.component.css']
+  styleUrls: ['./creacion-preguntas-component.component.css'],
 })
 export class CreacionPreguntasComponentComponent implements OnInit {
-  title = "Agregar Pregunta"
+  title = 'Agregar Pregunta';
   tiposPregunta: string[] = [];
   areasConocimiento: string[] = [];
   descriptores: string[] = [];
   opcionesTipoPregunta: string[] = [
-    "Opción múltiple", "Única opción", "Verdadero o falso"
+    'Opción múltiple',
+    'Única opción',
+    'Verdadero o falso',
   ];
   opcionesAreaConocimiento: string[] = [
-    "Client-side", "Server-side", "Buenas prácticas", "Metodología"
+    'Client-side',
+    'Server-side',
+    'Buenas prácticas',
+    'Metodología',
   ];
-  opcionesDescriptores: string[] = [
-    "Programación reactiva"
-  ];
+  opcionesDescriptores: string[] = ['Programación reactiva'];
   opciones: string[] = [];
-  tipoPregunta: string = "Seleccione una opcion";
-  areaConocimiento: string = "Seleccione una opcion";
-  descriptor: string = "Seleccione una opcion"
+  tipoPregunta: string = 'Seleccione una opcion';
+  areaConocimiento: string = 'Seleccione una opcion';
+  descriptor: string = 'Seleccione una opcion';
   pregunta?: string;
   opcion?: string;
 
-  constructor(private cookieService: CookieService) { }
+  constructor(private cookieService: CookieService) {}
 
   ngOnInit(): void {
     if (this.cookieService.get('tipoPregunta') !== '') {
-      this.tipoPregunta = this.cookieService.get('tipoPregunta')
+      this.tipoPregunta = this.cookieService.get('tipoPregunta');
     }
     if (this.cookieService.get('areaConocimiento') !== '') {
-      this.areaConocimiento = this.cookieService.get('areaConocimiento')
+      this.areaConocimiento = this.cookieService.get('areaConocimiento');
     }
     if (this.cookieService.get('descriptor') !== '') {
-      this.descriptor = this.cookieService.get('descriptor')
+      this.descriptor = this.cookieService.get('descriptor');
     }
     if (JSON.parse(localStorage.getItem('opciones')!)) {
-      this.opciones = JSON.parse(localStorage.getItem('opciones')!)
+      this.opciones = JSON.parse(localStorage.getItem('opciones')!);
     }
-    this.pregunta = this.cookieService.get('pregunta')
+    this.pregunta = this.cookieService.get('pregunta');
   }
 
   persistirOpcion(namekey: string, value: string) {
-    this.cookieService.set(namekey, value, this.obtenerLimiteCookie(new Date()))
+    this.cookieService.set(
+      namekey,
+      value,
+      this.obtenerLimiteCookie(new Date())
+    );
   }
 
   obtenerLimiteCookie(fecha: Date): Date {
@@ -54,14 +61,25 @@ export class CreacionPreguntasComponentComponent implements OnInit {
   }
 
   agregarOpcion() {
-    this.opciones.push(this.opcion!)
-    localStorage.setItem("opciones", JSON.stringify(this.opciones))
-    this.opcion = "";
+    this.opciones.push(this.opcion!);
+    localStorage.setItem('opciones', JSON.stringify(this.opciones));
+    this.opcion = '';
+  }
+
+  eliminarOpcion(opcion: string) {
+    let item = this.opciones.findIndex((element) => element == opcion);
+    this.opciones.splice(item, 1);
+    localStorage.setItem('opciones', JSON.stringify(this.opciones));
+    console.log(item);
+  }
+
+  editarOpcion(indice: number) {
+    this.opcion = this.opciones[indice];
+    console.log(this.opciones[indice]);
   }
 
   guardarPregunta() {
-    this.cookieService.deleteAll('/')
-    localStorage.removeItem('opciones')
+    this.cookieService.deleteAll('/');
+    localStorage.removeItem('opciones');
   }
-
 }
