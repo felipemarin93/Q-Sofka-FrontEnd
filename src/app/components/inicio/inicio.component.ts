c9a0e82b95d1ec82131ca4c47387ec3773dfd572
duvanleal96 <duvanleal65@gmail.com>
2022-08-09 11:20:31 -0500

    guardar datos local storage

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { subscribeOn } from 'rxjs';
 import { Usuario } from 'src/app/models/usuario';
 import { AutenticacionInicioSesionService } from 'src/app/services/autenticacion-inicio-sesion.service';
 
@@ -9,10 +10,12 @@ import { AutenticacionInicioSesionService } from 'src/app/services/autenticacion
   styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  userData: any;
   constructor(
     private autenticacionInicioSesion: AutenticacionInicioSesionService,
     private router: Router
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {}
 
@@ -25,6 +28,7 @@ export class InicioComponent implements OnInit {
             if (usuario1 != null) {
               if (usuario1.contrasena === contrasena) {
                 this.router.navigate(['coach-dashboard']);
+                localStorage.setItem("user", nombreUsuario)
               } else {
                 alert('La contraseña es incorrecta');
               }
@@ -41,4 +45,5 @@ export class InicioComponent implements OnInit {
       alert('El usuario y la contraseña no pueden estar vacíos');
     }
   }
+ 
 }
