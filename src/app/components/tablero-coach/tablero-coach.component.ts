d4a641b33239032fbcfd389db698a9add6d53edd
juan9507 <juandavidnaranjo75@gmail.com>
2022-08-09 11:41:57 -0500

    se cofiguro la ruta para editar la pregunta

--- src/app/components/tablero-coach/tablero-coach.component.ts
+++ src/app/components/tablero-coach/tablero-coach.component.ts
@@ -3,63 +3,60 @@ import { PreguntasService } from '../../services/preguntas.service';
 import { Pregunta } from '../../models/pregunta';
 import { Router, RouterLink } from '@angular/router';
 
-
 @Component({
   selector: 'app-tablero-coach',
   templateUrl: './tablero-coach.component.html',
-  styleUrls: ['./tablero-coach.component.css']
+  styleUrls: ['./tablero-coach.component.css'],
 })
 export class TableroCoachComponent implements OnInit {
-
   preguntas: Pregunta[] = [];
 
   pagina: number = 1;
-  title:string = "Bienvenido/a Coach";
-  preguntaDetalle?:Pregunta;
-  displayModal ="none";
+  title: string = 'Bienvenido/a Coach';
+  preguntaDetalle?: Pregunta;
+  displayModal = 'none';
 
   constructor(
     private preguntasService: PreguntasService,
-    private router: Router) { }
-
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-      this.getPreguntas();
+    this.getPreguntas();
   }
   getPreguntas(): void {
-    this.preguntasService.getPreguntas()
-    .subscribe(preguntas => {
+    this.preguntasService.getPreguntas().subscribe((preguntas) => {
       this.preguntas = preguntas;
       console.log(preguntas);
     });
   }
 
-
-  mostrarDetalle(preguntaActual:Pregunta){
+  mostrarDetalle(preguntaActual: Pregunta) {
     this.preguntaDetalle = preguntaActual;
-    this.displayModal= "block";
+    this.displayModal = 'block';
   }
 
-  cerrarDetalle(){
-    this.displayModal = "none";
+  cerrarDetalle() {
+    this.displayModal = 'none';
   }
 
-  cerrarSesion(){
-    let sesion= window.confirm("¿seguro que deseas salir?")
-    if(sesion==true){
+  cerrarSesion() {
+    let sesion = window.confirm('¿seguro que deseas salir?');
+    if (sesion == true) {
       this.router.navigate(['inicio']);
     }
   }
 
-  nuevaPregunta(){
+  nuevaPregunta() {
     this.router.navigate(['creacionpreguntas']);
   }
 
-  eliminarPregunta(preguntaEliminar:Pregunta){
-      alert('DELETE: ' + preguntaEliminar.id);
+  eliminarPregunta(preguntaEliminar: Pregunta) {
+    alert('DELETE: ' + preguntaEliminar.id);
   }
 
-  editarPregunta(preguntaEditar:Pregunta){
-    alert('IR A VISTA EDITAR: : ' + preguntaEditar.id);
+  editarPregunta(preguntaEditar: Pregunta) {
+    this.router.navigate(['creacionpreguntas/' + preguntaEditar.id]);
+    // alert('IR A VISTA EDITAR: : ' + preguntaEditar.id);
   }
 }
