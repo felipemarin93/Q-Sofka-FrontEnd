99617a32be6cf3553fa96d02a0b1202ce24c279e
miller gallego <miller.gallegof@gmail.com>
2022-08-11 12:41:18 -0500

    Se crean servicio para solicitar puntaje del back

--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-resultado',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resultado.component.css']
 })
 export class ResultadoComponent implements OnInit {
+  idPregunta?: string;
+  puntosPrueba1?: number;
+  puntosPrueba2?: number;
   title = '¡Hola, futuro sofkiano!';
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.idPregunta = this.activateRoute.snapshot.params['id'];
   }
 
 }
