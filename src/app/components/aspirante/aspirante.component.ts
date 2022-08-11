7343e92c68d092cbd906c219c9867abc83c255ac
mmaurogg <mmaurogg@gmail.com>
2022-08-10 21:04:05 -0500

    merge rama dev

--- src/app/components/aspirante/aspirante.component.ts
+++ src/app/components/aspirante/aspirante.component.ts
@@ -1,39 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { map, Observable } from 'rxjs';
-import { Aspirante } from 'src/app/models/aspirante';
-import { AspiranteService } from 'src/app/services/aspirante.service';
-=======
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-import { Observable } from 'rxjs';
->>>>>>> dev
+import { map, Observable } from 'rxjs';
+import { Aspirante } from 'src/app/models/aspirante';
+import { AspiranteService } from 'src/app/services/aspirante.service';
 
 @Component({
   selector: 'app-aspirante',
   templateUrl: './aspirante.component.html',
-  styleUrls: ['./aspirante.component.css'],
+  styleUrls: ['./aspirante.component.css']
 })
+
 export class AspiranteComponent implements OnInit {
+
   formaDatos: FormGroup | any;
   formaCodigo: FormGroup | any;
 
-<<<<<<< HEAD
   //codigoValido: boolean = false;
 
   constructor( private fb: FormBuilder,
-              private aspiranteService: AspiranteService ) {
+              private aspiranteService: AspiranteService,
+              private cookieService: CookieService,
+              private router: Router ) {
     this.crearFormulario();
     //this.crearListeners();
    }
-=======
-  constructor(private fb: FormBuilder, private cookieService: CookieService, private router: Router) {
-    this.crearFormulario();
-    this.crearListeners();
-  }
->>>>>>> dev
 
   ngOnInit(): void {}
 
@@ -71,7 +63,6 @@ export class AspiranteComponent implements OnInit {
     })
 
     this.formaCodigo = this.fb.group({
-<<<<<<< HEAD
       codigo:['', [Validators.required, Validators.minLength(5)],]
     })
   }
@@ -89,14 +80,6 @@ export class AspiranteComponent implements OnInit {
     return this.formaCodigo.get('codigo').invalid && this.formaCodigo.get('codigo').touched;
   }
 
-
-  // crearListeners(){
-  //   this.formaCodigo.valueChanges.subscribe( (valor: any) => console.log(valor)
-  //   )
-
-  //   this.formaCodigo.statusChanges.subscribe( (status: any) => console.log({status})
-  //   )
-  // }
 
   solicitarCodigo(){
     if(this.formaDatos.invalid){
@@ -148,69 +131,7 @@ export class AspiranteComponent implements OnInit {
 
   obtenerAspirante(codigoVerificacion: string){
     return this.aspiranteService.obtenerAspirantePorCodigoVerificacion(codigoVerificacion)
-=======
-      codigo: [
-        '',
-        [Validators.required, Validators.minLength(5)],
-        [this.validarCodigo],
-      ],
-    });
   }
 
-  crearListeners() {
-    this.formaCodigo.valueChanges.subscribe((valor: any) => console.log(valor));
-
-    this.formaCodigo.statusChanges.subscribe((status: any) =>
-      console.log({ status })
-    );
->>>>>>> dev
-  }
-
-  get nombreNoValido() {
-    return (
-      this.formaDatos.get('nombre').invalid &&
-      this.formaDatos.get('nombre').touched
-    );
-  }
-
-  get emailNoValido() {
-    return (
-      this.formaDatos.get('email').invalid &&
-      this.formaDatos.get('email').touched
-    );
-  }
-
-  get codigoNoValido() {
-    return (
-      this.formaCodigo.get('codigo').invalid &&
-      this.formaCodigo.get('codigo').touched
-    );
-  }
-
-  solicitarCodigo() {
-    if (this.formaDatos.invalid) {
-      Object.values(this.formaDatos.controls).forEach((control: any) => {
-        control.markAsTouched();
-      });
-    }
-  }
-
-  validarCodigo(control: FormControl): Promise<any> | Observable<any> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'abc1234') {
-          console.log('token valido');
-
-          resolve({ existe: false });
-        } else {
-          console.log('token no valido');
-        }
-      }, 1500);
-    });
-  }
-
-  comenzar() {
-    console.log(this.formaCodigo);
-  }
 }
 
